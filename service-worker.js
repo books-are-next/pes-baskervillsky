72e3d854c1ffe6176b28d37adf56dee6847f1ae5
jan-martinek <jan-martinek@users.noreply.github.com>
2022-05-12 15:11:31 +0000

    Deploying to docs from @ books-are-next/pes-baskervillsky@f67e58de64cdfbb79a31b3fc7dded54e32704e96 🚀

--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-ec22fab';
+const CACHE = 'cache-f67e58d';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
@@ -1019,7 +1019,7 @@ async function cache(context) {
         .open(CACHE)
         .then(cache => {
         console.log('[' + context + '] Opened cache');
-        return cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./pes_baskervillsky_split_000.html","./pes_baskervillsky_split_003.html","./pes_baskervillsky_split_004.html","./pes_baskervillsky_split_005.html","./pes_baskervillsky_split_006.html","./pes_baskervillsky_split_007.html","./pes_baskervillsky_split_008.html","./pes_baskervillsky_split_009.html","./pes_baskervillsky_split_010.html","./pes_baskervillsky_split_011.html","./pes_baskervillsky_split_012.html","./pes_baskervillsky_split_013.html","./pes_baskervillsky_split_014.html","./pes_baskervillsky_split_015.html","./pes_baskervillsky_split_016.html","./pes_baskervillsky_split_017.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001.jpg","./resources/image002.jpg","./resources/image003.gif","./resources/image004.gif","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]);
+        return cache.addAll(["./","./colophon.html","./favicon.png","./manifest.json","./index.html","./pes_baskervillsky_split_000.html","./pes_baskervillsky_split_003.html","./pes_baskervillsky_split_004.html","./pes_baskervillsky_split_005.html","./pes_baskervillsky_split_006.html","./pes_baskervillsky_split_007.html","./pes_baskervillsky_split_008.html","./pes_baskervillsky_split_009.html","./pes_baskervillsky_split_010.html","./pes_baskervillsky_split_011.html","./pes_baskervillsky_split_012.html","./pes_baskervillsky_split_013.html","./pes_baskervillsky_split_015.html","./pes_baskervillsky_split_014.html","./pes_baskervillsky_split_016.html","./pes_baskervillsky_split_017.html","./promo.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001.jpg","./resources/image002.jpg","./resources/image003.gif","./resources/image004.gif","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]);
     })
         .then(() => idb.set({ key: 'cacheUpdatedAt', value: new Date().toISOString() }))
         .then(() => {
